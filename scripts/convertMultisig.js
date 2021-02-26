6b9df45bb58f160c041aedf06907366cc35308ce
planethouki <26595148+planethouki@users.noreply.github.com>
2021-02-27 08:31:52 +0900

    convert multisig script

--- scripts/convertMultisig.js
+++ scripts/convertMultisig.js
@@ -29,114 +29,121 @@ Alice
      └─Dave
  */
 
-const networkType = NetworkType.TEST_NET
-const generationHash = process.env.GENERATION_HASH
-const mosaicId = process.env.MOSAIC_ID
+const main = async () => {
 
-const nodeList = process.env.NODE_LIST_CSV.split(',')
+  const url = process.env.NODE_URL
 
-const url = nodeList[0]
-const initiator = Account.createFromPrivateKey(
-  process.env.PRIVATE_KEY,
-  networkType
-)
+  const repositoryFactory = new RepositoryFactoryHttp(url)
+  const transactionHttp = repositoryFactory.createTransactionRepository()
+  const transactionStatusHttp = repositoryFactory.createTransactionStatusRepository()
 
-const alice = Account.generateNewAccount(networkType)
-const bob = Account.generateNewAccount(networkType)
-const carol = Account.generateNewAccount(networkType)
-const dave = Account.generateNewAccount(networkType)
-const ellen = Account.generateNewAccount(networkType)
-const frank = Account.generateNewAccount(networkType)
-const steve = Account.generateNewAccount(networkType)
-const trent = Account.generateNewAccount(networkType)
+  const networkType = await repositoryFactory.getNetworkType().toPromise()
+  const generationHash = await repositoryFactory.getGenerationHash().toPromise()
+  const mosaicId = (await repositoryFactory.getCurrencies().toPromise()).currency.mosaicId.toHex()
+  const epochAdjustment = await repositoryFactory.getEpochAdjustment().toPromise()
 
-const accounts = [alice, bob, carol, dave, ellen, frank, steve, trent]
+  const initiator = Account.createFromPrivateKey(
+    process.env.PRIVATE_KEY,
+    networkType
+  )
 
-const dummyTx = TransferTransaction.create(
-  Deadline.create(),
-  initiator.address,
-  [new Mosaic(new MosaicId(mosaicId), UInt64.fromUint(0))],
-  PlainMessage.create(''),
-  networkType
-)
+  const alice = Account.generateNewAccount(networkType)
+  const bob = Account.generateNewAccount(networkType)
+  const carol = Account.generateNewAccount(networkType)
+  const dave = Account.generateNewAccount(networkType)
+  const ellen = Account.generateNewAccount(networkType)
+  const frank = Account.generateNewAccount(networkType)
+  const steve = Account.generateNewAccount(networkType)
+  const trent = Account.generateNewAccount(networkType)
 
-const steveConvertTx = MultisigAccountModificationTransaction.create(
-  Deadline.create(),
-  2,
-  2,
-  [trent, dave].map((co) => co.address),
-  [],
-  networkType
-)
-const ellenConvertTx = MultisigAccountModificationTransaction.create(
-  Deadline.create(),
-  2,
-  2,
-  [frank, steve].map((co) => co.address),
-  [],
-  networkType
-)
-const bobConvertTx = MultisigAccountModificationTransaction.create(
-  Deadline.create(),
-  2,
-  2,
-  [carol, dave].map((co) => co.address),
-  [],
-  networkType
-)
-const aliceConvertTx = MultisigAccountModificationTransaction.create(
-  Deadline.create(),
-  2,
-  2,
-  [bob, ellen].map((co) => co.address),
-  [],
-  networkType
-)
-const aggregateTx = AggregateTransaction.createComplete(
-  Deadline.create(),
-  [
-    dummyTx.toAggregate(initiator.publicAccount),
-    steveConvertTx.toAggregate(steve.publicAccount),
-    ellenConvertTx.toAggregate(ellen.publicAccount),
-    bobConvertTx.toAggregate(bob.publicAccount),
-    aliceConvertTx.toAggregate(alice.publicAccount),
-  ],
-  networkType,
-  [],
-  UInt64.fromUint(200000)
-)
-const aggregateSignedTx = initiator.signTransactionWithCosignatories(
-  aggregateTx,
-  accounts,
-  generationHash
-)
+  const accounts = [alice, bob, carol, dave, ellen, frank, steve, trent]
 
-const repositoryFactory = new RepositoryFactoryHttp(url)
-const transactionHttp = repositoryFactory.createTransactionRepository()
-const transactionStatusHttp = repositoryFactory.createTransactionStatusRepository()
+  const dummyTx = TransferTransaction.create(
+    Deadline.create(epochAdjustment),
+    initiator.address,
+    [new Mosaic(new MosaicId(mosaicId), UInt64.fromUint(0))],
+    PlainMessage.create(''),
+    networkType
+  )
 
-transactionHttp
-  .announce(aggregateSignedTx)
-  .toPromise()
-  .then(() => {
-    return new Promise((resolve) => setTimeout(resolve, 30000))
+  const steveConvertTx = MultisigAccountModificationTransaction.create(
+    Deadline.create(epochAdjustment),
+    2,
+    2,
+    [trent, dave].map((co) => co.address),
+    [],
+    networkType
+  )
+  const ellenConvertTx = MultisigAccountModificationTransaction.create(
+    Deadline.create(epochAdjustment),
+    2,
+    2,
+    [frank, steve].map((co) => co.address),
+    [],
+    networkType
+  )
+  const bobConvertTx = MultisigAccountModificationTransaction.create(
+    Deadline.create(epochAdjustment),
+    2,
+    2,
+    [carol, dave].map((co) => co.address),
+    [],
+    networkType
+  )
+  const aliceConvertTx = MultisigAccountModificationTransaction.create(
+    Deadline.create(epochAdjustment),
+    2,
+    2,
+    [bob, ellen].map((co) => co.address),
+    [],
+    networkType
+  )
+  const aggregateTx = AggregateTransaction.createComplete(
+    Deadline.create(epochAdjustment),
+    [
+      dummyTx.toAggregate(initiator.publicAccount),
+      steveConvertTx.toAggregate(steve.publicAccount),
+      ellenConvertTx.toAggregate(ellen.publicAccount),
+      bobConvertTx.toAggregate(bob.publicAccount),
+      aliceConvertTx.toAggregate(alice.publicAccount),
+    ],
+    networkType,
+    [],
+    UInt64.fromUint(200000)
+  )
+  const aggregateSignedTx = initiator.signTransactionWithCosignatories(
+    aggregateTx,
+    accounts,
+    generationHash
+  )
+
+  transactionHttp
+    .announce(aggregateSignedTx)
+    .toPromise()
+    .then(() => {
+      return new Promise((resolve) => setTimeout(resolve, 30000))
+    })
+    .then(() => {
+      return transactionStatusHttp
+        .getTransactionStatus(aggregateSignedTx.hash)
+        .toPromise()
+    })
+    .then((result) => {
+      console.log(`hash: ${aggregateSignedTx.hash}`)
+      console.log(`status: ${JSON.stringify(result)}`)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+
+  accounts.map((a, index) => {
+    console.log(`account ${index + 1}`)
+    console.log(`private: ${a.privateKey}`)
+    console.log(`public : ${a.publicKey}`)
+    console.log(`address: ${a.address.plain()}`)
   })
-  .then(() => {
-    return transactionStatusHttp
-      .getTransactionStatus(aggregateSignedTx.hash)
-      .toPromise()
-  })
-  .then((result) => {
-    console.log(`hash: ${aggregateSignedTx.hash}`)
-    console.log(`status: ${JSON.stringify(result)}`)
-  })
-  .catch((e) => {
-    console.log(e)
-  })
 
-accounts.map((a, index) => {
-  console.log(`account ${index + 1}`)
-  console.log(`private: ${a.privateKey}`)
-  console.log(`public : ${a.publicKey}`)
-  console.log(`address: ${a.address.plain()}`)
-})
+
+}
+
+main()
